1802e033a44153a931fc391e5604ae613e761d09
妖怪不会飞 <contact@yangjinkun.cn>
2015-07-29 21:27:36 +0800

    Update index.js

--- core/index.js
+++ core/index.js
@@ -3,7 +3,7 @@
 var server = require('./server');
 
 // Set the default environment to be `development`
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV =  'production';
 
 function makeGhost(options) {
     options = options || {};
